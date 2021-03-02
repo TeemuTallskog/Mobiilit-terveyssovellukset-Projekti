7631e4e1ab3cf80152cae74070fcbaef559b3d27
studud <71371760+studud@users.noreply.github.com>
2021-03-02 09:39:39 +0200

    Update SettingsActivity.java

--- app/src/main/java/com/example/raskaussovellus/SettingsActivity.java
+++ app/src/main/java/com/example/raskaussovellus/SettingsActivity.java
@@ -1,17 +1,27 @@
 package com.example.raskaussovellus;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
     }
-}
+
+    public void launchActivity(View view){
+        Intent intent = new Intent(this, Alarm.class);
+        startActivity(intent);
+
+    }
+
+    }
+
