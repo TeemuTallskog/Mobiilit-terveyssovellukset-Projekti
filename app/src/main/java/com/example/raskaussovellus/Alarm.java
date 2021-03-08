d1c8379b64c273bcf66a4460050c65b698ed0d62
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2021-03-08 17:38:39 +0200

    Revert "Revert "name list""
    This reverts commit c9821f5e4750bf02c24d27ac4f99a1718194018d.

--- app/src/main/java/com/example/raskaussovellus/Alarm.java
+++ app/src/main/java/com/example/raskaussovellus/Alarm.java
@@ -60,7 +60,6 @@ public class Alarm extends AppCompatActivity {
                     // Edit Text shows hour and minute
                     timeHour.setText(String.format("%02d", hourOfDay));
                     timeMinute.setText(String.format("%02d", minute));
-
                 }
 
                 /**
---
+++ app/src/main/java/com/example/raskaussovellus/MaleNamesActivity.java
@@ -0,0 +1,68 @@
+package com.example.raskaussovellus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MaleNamesActivity extends AppCompatActivity {
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_male);
+
+        listView =(ListView)findViewById(R.id.lvMale);
+
+        /**
+         * Creates ne list and add names to it
+         */
+        ArrayList<String> arrayList= new ArrayList<>();
+        arrayList.add("Juhani");
+        arrayList.add("Olavi");
+        arrayList.add("Johannes");
+        arrayList.add("Mikael");
+        arrayList.add("Oliver");
+        arrayList.add("Ilmari");
+        arrayList.add("Juhani");
+        arrayList.add("Onni");
+        arrayList.add("Elias");
+        arrayList.add("Eino");
+        arrayList.add("Matias");
+        arrayList.add("Antero");
+        arrayList.add("Leo");
+        arrayList.add("Toivo");
+        arrayList.add("Väinö");
+        arrayList.add("Viljami");
+        arrayList.add("Tapio");
+        arrayList.add("Tapani");
+        arrayList.add("Oskari");
+        arrayList.add("Emil");
+        arrayList.add("Eemil");
+        arrayList.add("Veikko");
+        arrayList.add("Samuel");
+        arrayList.add("Eemeli");
+        arrayList.add("Benjamin");
+        arrayList.add("Kristian");
+        arrayList.add("Eeli");
+        arrayList.add("Armas");
+        arrayList.add("Armas");
+        arrayList.add("Noel");
+        arrayList.add("Alvar");
+        arrayList.add("Vilho");
+        arrayList.add("Kalevi");
+        arrayList.add("Aatos");
+        arrayList.add("Sakari");
+
+        /**
+         * list layout type
+         */
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+    }
+}
